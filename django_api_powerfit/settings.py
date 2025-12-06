d4f93669979dd6e3b648c7c2066c1e27faa16a7e
Ben11S <benjaminsotoferreira@gmail.com>
2025-12-06 19:09:28 -0300

    configuracion y adaptacion de BD en la nube con Azure

--- django_api_powerfit/settings.py
+++ django_api_powerfit/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'django_api_powerfit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  
+        'USER': 'powerfit_admin',
+        'PASSWORD': 'PoFit12@',
+        'HOST': 'powerfit-db-server.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',  
+        },
     }
 }
 
